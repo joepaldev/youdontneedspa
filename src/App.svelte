94cf69b0bc0ae9a4a9aec08b7b4003b0446bb6d9
joepal.dev <2881953+joepaldev@users.noreply.github.com>
2020-08-30 08:07:02 +0000

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  const name = "pwa";
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>svelte pwa template</title>
+</svelte:head>
+
+<main>
+  <h1>hello {name}!</h1>
+  <img src="/assets/android-chrome-192x192.png" alt="svelte_icon" />
+</main>
